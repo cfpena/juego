664f8ce56b793810063afa5bc991a13a5233a782
cfpena <cfpena@espol.edu.ec>
2015-10-02 00:33:19 -0500

    ganar desarrollo

--- src/GameOverDesarrollo.js
+++ src/GameOverDesarrollo.js
@@ -5,26 +5,28 @@ Ball.GameOverDesarrollo.prototype = {
 			
 			prestamo = this.add.tileSprite(0, 0,800,600, 'fondo_carrera');
 
-			botonVerde = this.add.button(0, 550, 'botonAtlas', emit2.action1, this, 'over', 'out', 'down');
-			botonRojo = this.add.button(200, 550, 'botonAtlas', emit.seguir, this, 'over', 'out', 'down');
-			botonAmarillo = this.add.button(400, 550, 'botonAtlas', emit.menu, this, 'over', 'out', 'down');
+			botonVerde = this.add.button(0, 550, 'botonAtlas', emit3.action1, this, 'over', 'out', 'down');
+			botonRojo = this.add.button(200, 550, 'botonAtlas', emit3.action2, this, 'over', 'out', 'down');
+			botonAmarillo = this.add.button(400, 550, 'botonAtlas', emit3.action3, this, 'over', 'out', 'down');
 			
 			
 		    
 
 		}};
-emit2 = {
+emit3 = {
 	action1 :function(){
 		velocidad=20;
 		
 		this.game.state.start('Desarrollo');
 	},
 action2 :function(){
+	
+	this.game.state.start('Desarrollo');
 		
 		
 	},
 action3 :function(){
-		
+	this.game.state.start('Mapa');
 		
 	},
 		
--- src/Preloader.js
+++ src/Preloader.js
@@ -1,4 +1,4 @@
-var velocidad=60;
+var velocidad=10;
 var prestamoDesarrollo=false;
 Ball.Preloader = function(game) {};
 Ball.Preloader.prototype = {
@@ -20,21 +20,21 @@ Ball.Preloader.prototype = {
 		this.load.image('border-horizontal', 'img/border-horizontal.png');
 		this.load.image('border-vertical', 'img/border-vertical.png');
 		this.load.image('fondo2', 'img/fondo2.jpg');
-		this.load.image('fondo_carrera', 'resources/images/ESCENARIO 2.png');
+		this.load.image('fondo_carrera', 'resources/ESCENARIO 2.png');
 
 		//this.game.load.spritesheet('planeta', 'img/planeta.png', 193, 71);
 		this.game.load.spritesheet('planeta', 'img/planeta.png');
 		this.game.load.image('planetaSprite', 'img/planeta.png');
 
-		this.load.image('nave-1', 'resources/images/nave-1.png');
-		this.load.image('nave-2', 'resources/images/nave-2.png');
-		this.load.image('nave-3', 'resources/images/nave-3.png');
-		this.load.image('nave-4', 'resources/images/nave-4.png');
+		
 		this.load.spritesheet('Asteroides', 'img/Asteroides.png',64, 64);
 		this.load.spritesheet('button-audio', 'img/button-audio.png', 35, 35);
 		this.load.spritesheet('button-start', 'img/button-start.png', 146, 51);
 		this.load.atlas('botonAtlas', 'img/button_texture_atlas.png', 'src/button_texture_atlas.json');
-		this.load.spritesheet('block', 'img/block.png', 95, 95);
+		this.load.image('block', 'img/block.png', 95, 95);
+		this.load.image('robot1', 'resources/robot1.png', 166, 253);
+		this.load.image('robot2', 'resources/robot2.png', 167, 254);
+		this.load.image('robot3', 'resources/robot3.png', 166, 248);
 
 
 
