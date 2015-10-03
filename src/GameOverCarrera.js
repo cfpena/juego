3c9568c571d49e66f34c1799e043fdeb3c584da0
jjjohncedeno <jjjohncedeno@gmail.com>
2015-10-03 09:28:25 -0500

    Añadir mensajes en minijuego carrera.

--- src/GameOverCarrera.js
+++ src/GameOverCarrera.js
@@ -3,27 +3,27 @@ Ball.GameOverCarrera.prototype = {
 
 		create: function() {
 
-			this.add.tileSprite(0, 0,800,600, 'screen-gameoverc');
 			if (!NaveNueva){
-				botonVerde = this.add.button(200, 400, 'botonAtlas', this.irCarreraNueva, this, 'over', 'out', 'down');
-				this.add.text(432, 407, 'NaveNueva', { font: '20px Arial', fill: '#ffffff' })
+				this.add.tileSprite(0, 0,800,600, 'screen-gameoverrenova');
+				botonRojo = this.add.button(400, 400, 'botonAtlas', this.irCarrera, this, 'over', 'out', 'down');
+				this.add.text(432, 407, 'Si', { font: '20px Arial', fill: '#ffffff' })
+
+				botonAmarillo = this.add.button(600, 400, 'botonAtlas', this.irMapa, this, 'over', 'out', 'down');
+				this.add.text(632, 407, 'No', { font: '20px Arial', fill: '#ffffff' })
 			}else{
+				this.add.tileSprite(0, 0,800,600, 'screen-gameover2');
+				botonRojo = this.add.button(400, 400, 'botonAtlas', this.irCarrera, this, 'over', 'out', 'down');
+				this.add.text(432, 407, 'Volver a Jugar', { font: '20px Arial', fill: '#ffffff' })
 
+				botonAmarillo = this.add.button(600, 400, 'botonAtlas', this.irMapa, this, 'over', 'out', 'down');
+				this.add.text(632, 407, 'Mapa', { font: '20px Arial', fill: '#ffffff' })
 			}
-			botonRojo = this.add.button(400, 400, 'botonAtlas', this.irCarrera, this, 'over', 'out', 'down');
-			this.add.text(432, 407, 'Volver a Jugar', { font: '20px Arial', fill: '#ffffff' })
 
-			botonAmarillo = this.add.button(600, 400, 'botonAtlas', this.irMapa, this, 'over', 'out', 'down');
-			this.add.text(632, 407, 'Mapa', { font: '20px Arial', fill: '#ffffff' })
-
-		},
-		irCarreraNueva:function(){
-			this.game.state.start('Carrera');
-			NaveNueva = true;
-			EscenarioNave = 2;
 		},
 		irCarrera :function(){
 			this.game.state.start('Carrera');
+			NaveNueva = true;
+			EscenarioNave = 2;
 		},
 		irMapa :function(){
 			this.game.state.start('Mapa');
