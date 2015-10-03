7001adf99afd87cd2b28993786ea5df1f31b958f
cfpena <cfpena@espol.edu.ec>
2015-10-03 04:42:22 -0500

    windesarrollo

--- src/WinDesarrollo.js
+++ src/WinDesarrollo.js
@@ -4,6 +4,9 @@ Ball.WinDesarrollo.prototype = {
 		create: function() {
 
 			prestamo = this.add.tileSprite(0, 0,800,600, 'fondo_carrera');
+			this.add.text(30, 30, '           HAS GANADO\N Derrotaste a Malena, pero aun puedes seguir produciendo,\n quieres seguir?', { font: '42px Arial', fill: '#ffffff' })
+
+			
 
 			botonRojo = this.add.button(400, 400, 'botonAtlas', emit3.action2, this, 'over', 'out', 'down');
 			this.add.text(432, 407, 'Seguir', { font: '42px Arial', fill: '#ffffff' })
